62b96d33d8ff56b524c4e2f066401f3d10cf6410
Xing Zhou <xzhou@pivotal.io>
2018-05-19 21:51:02 -0400

    Refactor MainWindow

--- src/org/chartsmart/MainWindow.java
+++ src/org/chartsmart/MainWindow.java
@@ -1,138 +1,147 @@
 package org.chartsmart;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Insets;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class MainWindow extends JPanel {
+    private JButton showChartButton;
+    private JButton showPieOrBarChartButton;
+    private JComboBox chartTypeSelector;
+    private JComboBox displayTypeSelector;
 
-public class MainWindow extends JPanel
-{
-  private JButton   btnShowBarChart;
-  private JButton   btnShowPieOrBarChart;
-  private JButton   btnShowSingleOrComparechart;
-  private JComboBox cboChartType;
-  private JComboBox cboDisplayType;
-  public MainWindow()
-  {
-    initializeLayout();
-  }
-  public void showGui()
-  {
-    JFrame frame = new JFrame("ChartSmart, the Smart way to do Charts.");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(this);
-    Insets insets = frame.getInsets();
-    frame.setSize(500 + insets.left + insets.right, 500 + insets.top + insets.bottom);
-    frame.setLocation(100, 100);
-    frame.setVisible(true);
-  }
-  private void initializeLayout()
-  {
-    this.setPreferredSize(new Dimension(500, 500));
-    this.setLayout(null);
-    btnShowBarChart = new JButton("Show Chart");
-    btnShowPieOrBarChart = new JButton("Show Pie or Bar");
-    btnShowSingleOrComparechart = new JButton("Show Display Type");
-    add(btnShowBarChart);
-    add(btnShowPieOrBarChart);
-    add(btnShowSingleOrComparechart);
-    String[] chartTypes = {"Bar Chart", "Pie Chart"};
-    cboChartType = new JComboBox(chartTypes);
-    String[] displayTypes = {"Single", "Compare"};
-    cboDisplayType = new JComboBox(displayTypes);
-    add(cboChartType);
-    add(cboDisplayType);
-    ButtonListener buttonListener = new ButtonListener();
-    btnShowBarChart.addActionListener(buttonListener);
-    btnShowPieOrBarChart.addActionListener(buttonListener);
-    btnShowSingleOrComparechart.addActionListener(buttonListener);
-    Insets insets = getInsets();
-    Dimension size = btnShowBarChart.getPreferredSize();
-    btnShowBarChart.setBounds(25 + insets.left, 25 + insets.top, size.width, size.height);
-    size = btnShowPieOrBarChart.getPreferredSize();
-    btnShowPieOrBarChart.setBounds(25 + insets.left, 65 + insets.top, size.width, size.height);
-    cboChartType.setBounds(200, 65 + insets.top, 200, size.height);
-    size = btnShowSingleOrComparechart.getPreferredSize();
-    btnShowSingleOrComparechart.setBounds(25 + insets.left, 105 + insets.top, size.width, size.height);
-    cboDisplayType.setBounds(200, 105 + insets.top, 200, size.height);
-  }
-  public static void main(String[] args)
-  {
-    javax.swing.SwingUtilities.invokeLater(new Runnable()
-    {
-      @Override
-      public void run()
-      {
-        MainWindow chartSmart = new MainWindow();
-        chartSmart.showGui();
-      }
-    });
-  }
-  class ButtonListener implements ActionListener
-  {
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-      if (e.getSource() == btnShowBarChart)
-      {
-        openBarChart();
-      }
-      else if (e.getSource() == btnShowPieOrBarChart)
-      {
-        DisplayCharts cw = new DisplayCharts();
-        cw.initializeChart(getChartType(), "rpfll");
-        display(cw);
-      }
-      else
-      {
-        DisplayCharts cw = new DisplayCharts();
-        cw.initializeChart(getChartType(), getDisplayType());
-        display(cw);
-      }
+    private void initializeLayout() {
+        this.setPreferredSize(new Dimension(500, 500));
+        this.setLayout(null);
+        showChartButton = new ShowChartButton(this);
+        showPieOrBarChartButton = new ShowPieOrBarChartButton(this);
+        chartTypeSelector = new BarOrPieChartSelector(this, showPieOrBarChartButton);
+        JButton showDisplayTypeButton = new ShowDisplayTypeButton(this);
+        displayTypeSelector = new DisplayTypeSelector(this, showDisplayTypeButton);
     }
-    private void openBarChart()
-    {
-      DisplayCharts cw = new DisplayCharts();
-      cw.initializeChart(406, "rpfll");
-      display(cw);
+
+    public MainWindow() {
+        initializeLayout();
     }
-    private void display(DisplayCharts cw)
-    {
-      JFrame frame = new JFrame();
-      frame.getContentPane().add(cw);
-      frame.setLocation(150, 150);
-      frame.setBackground(new Color(255, 239, 213));
-      frame.pack();
-      frame.setTitle(cw.getChartTitle());
-      frame.setVisible(true);
+
+    private void showGui() {
+        JFrame frame = new JFrame("ChartSmart, the Smart way to do Charts.");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(this);
+        Insets insets = frame.getInsets();
+        frame.setSize(500 + insets.left + insets.right, 500 + insets.top + insets.bottom);
+        frame.setLocation(100, 100);
+        frame.setVisible(true);
     }
-    private String getDisplayType()
-    {
-      if ((String) cboDisplayType.getSelectedItem() == "Single")
-      {
-        return "rpfll";
-      }
-      else
-      {
-        return "shareddisplay";
-      }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            MainWindow chartSmart = new MainWindow();
+            chartSmart.showGui();
+        });
     }
-    private int getChartType()
-    {
-      if ((String) cboChartType.getSelectedItem() == "Bar Chart")
-      {
-        return 406;
-      }
-      else
-      {
-        return 323;
-      }
+
+    private class ButtonListener implements ActionListener {
+        private boolean isSingleDisplayType(Object pressedButton, Object selectedDisplayType) {
+            return pressedButton == showChartButton
+                    || pressedButton == showPieOrBarChartButton
+                    || selectedDisplayType == "Single";
+        }
+
+        private String determineDisplayType(ActionEvent e) {
+            Object pressedButton = e.getSource();
+            Object selectedDisplayType = displayTypeSelector.getSelectedItem();
+            if (isSingleDisplayType(pressedButton, selectedDisplayType)) {
+                return "rpfll";
+            }
+
+            return "shareddisplay";
+        }
+
+        private boolean isBarChart(Object pressedButton, Object selectedChartType) {
+            return pressedButton == showChartButton || selectedChartType == "Bar Chart";
+        }
+
+        private int determineChartType(ActionEvent e) {
+            Object pressedButton = e.getSource();
+            Object selectedChartType = chartTypeSelector.getSelectedItem();
+            if (isBarChart(pressedButton, selectedChartType)) {
+                return 406;
+            }
+            return 323;
+        }
+
+        private void display(DisplayCharts chartWindow) {
+            JFrame frame = new JFrame();
+            frame.getContentPane().add(chartWindow);
+            frame.setLocation(150, 150);
+            frame.setBackground(new Color(255, 239, 213));
+            frame.pack();
+            frame.setTitle(chartWindow.getChartTitle());
+            frame.setVisible(true);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            int chartType = determineChartType(e);
+            String displayType = determineDisplayType(e);
+            DisplayCharts chartWindow = new DisplayCharts();
+            chartWindow.initializeChart(chartType, displayType);
+            display(chartWindow);
+        }
+
     }
-  }
+
+    private class ShowChartButton extends JButton {
+        ShowChartButton(JPanel parent) {
+            super("Show Chart");
+            this.addActionListener(new ButtonListener());
+            Dimension size = this.getPreferredSize();
+            Insets insets = parent.getInsets();
+            this.setBounds(25 + insets.left, 25 + insets.top, size.width, size.height);
+            parent.add(this);
+
+        }
+    }
+
+    private class ShowPieOrBarChartButton extends JButton {
+        ShowPieOrBarChartButton(JPanel parent) {
+            super("Show Pie or Bar");
+            this.addActionListener(new ButtonListener());
+            Dimension size = this.getPreferredSize();
+            Insets insets = parent.getInsets();
+            this.setBounds(25 + insets.left, 65 + insets.top, size.width, size.height);
+            parent.add(this);
+        }
+    }
+
+    private class BarOrPieChartSelector extends JComboBox<String> {
+        BarOrPieChartSelector(JPanel parent, JButton adjacent) {
+            super(new String[]{"Bar Chart", "Pie Chart"});
+            Dimension size = adjacent.getPreferredSize();
+            this.setBounds(200, 65 + parent.getInsets().top, 200, size.height);
+            parent.add(this);
+        }
+    }
+
+    private class ShowDisplayTypeButton extends JButton {
+        ShowDisplayTypeButton(JPanel parent) {
+            super("Show Display Type");
+            this.addActionListener(new ButtonListener());
+            Dimension size = this.getPreferredSize();
+            Insets insets = parent.getInsets();
+            this.setBounds(25 + insets.left, 105 + insets.top, size.width, size.height);
+            parent.add(this);
+
+        }
+    }
+
+    private class DisplayTypeSelector extends JComboBox<String> {
+        DisplayTypeSelector(JPanel parent, JButton adjacent) {
+            super(new String[]{"Single", "Compare"});
+            Insets insets = parent.getInsets();
+            this.setBounds(200, 105 + insets.top, 200, adjacent.getPreferredSize().height);
+            parent.add(this);
+        }
+    }
 }
