073f0b3d1663c9705a1c7b4c7b00dffb1fb18432
Ahmad Dandachi <adandac1@ford.com>
2018-08-03 13:59:16 -0400

    refactor: rename

--- src/org/chartsmart/Chart.java
+++ src/org/chartsmart/Chart.java
@@ -1,13 +1,7 @@
 package org.chartsmart;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class Chart extends JPanel {
 
